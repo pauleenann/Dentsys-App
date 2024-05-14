d4f286baa6e07906024136eed90f856f88feb7f1
Pau <141435859+pauleenann@users.noreply.github.com>
2024-05-14 13:15:58 +0800

    display selected tooth

--- frontend/src/Components/AddPatient/AddPatient.jsx
+++ frontend/src/Components/AddPatient/AddPatient.jsx
@@ -5,9 +5,145 @@ import AdminInfo from '../AdminInfo/AdminInfo'
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import tooth1 from './../../Assets/Tooth/tooth1.png'
+import tooth2 from './../../Assets/Tooth/tooth2.png'
+import tooth3 from './../../Assets/Tooth/tooth3.png'
+import tooth4 from './../../Assets/Tooth/tooth4.png'
+import tooth5 from './../../Assets/Tooth/tooth5.png'
+import tooth6 from './../../Assets/Tooth/tooth6.png'
+import tooth7 from './../../Assets/Tooth/tooth7.png'
+import tooth8 from './../../Assets/Tooth/tooth8.png'
+import tooth9 from './../../Assets/Tooth/tooth9.png'
+import tooth10 from './../../Assets/Tooth/tooth10.png'
+import tooth11 from './../../Assets/Tooth/tooth11.png'
+import tooth12 from './../../Assets/Tooth/tooth12.png'
+import tooth13 from './../../Assets/Tooth/tooth13.png'
+import tooth14 from './../../Assets/Tooth/tooth14.png'
+import tooth15 from './../../Assets/Tooth/tooth15.png'
+import tooth16 from './../../Assets/Tooth/tooth16.png'
+import tooth17 from './../../Assets/Tooth/tooth17.png'
+import tooth18 from './../../Assets/Tooth/tooth18.png'
+import tooth19 from './../../Assets/Tooth/tooth19.png'
+import tooth20 from './../../Assets/Tooth/tooth20.png'
+import tooth21 from './../../Assets/Tooth/tooth21.png'
+import tooth22 from './../../Assets/Tooth/tooth22.png'
+import tooth23 from './../../Assets/Tooth/tooth23.png'
+import tooth24 from './../../Assets/Tooth/tooth24.png'
+import tooth25 from './../../Assets/Tooth/tooth25.png'
+import tooth26 from './../../Assets/Tooth/tooth26.png'
+import tooth27 from './../../Assets/Tooth/tooth27.png'
+import tooth28 from './../../Assets/Tooth/tooth28.png'
+import tooth29 from './../../Assets/Tooth/tooth29.png'
+import tooth30 from './../../Assets/Tooth/tooth30.png'
+import tooth31 from './../../Assets/Tooth/tooth31.png'
+import tooth32 from './../../Assets/Tooth/tooth32.png'
+import selected1 from './../../Assets/Tooth Selected/selected1.png'
+import selected2 from './../../Assets/Tooth Selected/selected2.png'
+import selected3 from './../../Assets/Tooth Selected/selected3.png'
+import selected4 from './../../Assets/Tooth Selected/selected4.png'
+import selected5 from './../../Assets/Tooth Selected/selected5.png'
+import selected6 from './../../Assets/Tooth Selected/selected6.png'
+import selected7 from './../../Assets/Tooth Selected/selected7.png'
+import selected8 from './../../Assets/Tooth Selected/selected8.png'
+import selected9 from './../../Assets/Tooth Selected/selected9.png'
+import selected10 from './../../Assets/Tooth Selected/selected10.png'
+import selected11 from './../../Assets/Tooth Selected/selected11.png'
+import selected12 from './../../Assets/Tooth Selected/selected12.png'
+import selected13 from './../../Assets/Tooth Selected/selected13.png'
+import selected14 from './../../Assets/Tooth Selected/selected14.png'
+import selected15 from './../../Assets/Tooth Selected/selected15.png'
+import selected16 from './../../Assets/Tooth Selected/selected16.png'
+import selected17 from './../../Assets/Tooth Selected/selected17.png'
+import selected18 from './../../Assets/Tooth Selected/selected18.png'
+import selected19 from './../../Assets/Tooth Selected/selected19.png'
+import selected20 from './../../Assets/Tooth Selected/selected20.png'
+import selected21 from './../../Assets/Tooth Selected/selected21.png'
+import selected22 from './../../Assets/Tooth Selected/selected22.png'
+import selected23 from './../../Assets/Tooth Selected/selected23.png'
+import selected24 from './../../Assets/Tooth Selected/selected24.png'
+import selected25 from './../../Assets/Tooth Selected/selected25.png'
+import selected26 from './../../Assets/Tooth Selected/selected26.png'
+import selected27 from './../../Assets/Tooth Selected/selected27.png'
+import selected28 from './../../Assets/Tooth Selected/selected28.png'
+import selected29 from './../../Assets/Tooth Selected/selected29.png'
+import selected30 from './../../Assets/Tooth Selected/selected30.png'
+import selected31 from './../../Assets/Tooth Selected/selected31.png'
+import selected32 from './../../Assets/Tooth Selected/selected32.png'
+
 
 const AddPatient = () => {
+    const toothImages = {
+        1: { default: tooth1, selected: selected1 },
+        2: { default: tooth2, selected: selected2 },
+        3: { default: tooth3, selected: selected3 },
+        4: { default: tooth4, selected: selected4 },
+        5: { default: tooth5, selected: selected5 },
+        6: { default: tooth6, selected: selected6 },
+        7: { default: tooth7, selected: selected7 },
+        8: { default: tooth8, selected: selected8 },
+        9: { default: tooth9, selected: selected9 },
+        10: { default: tooth10, selected: selected10 },
+        11: { default: tooth11, selected: selected11 },
+        12: { default: tooth12, selected: selected12 },
+        13: { default: tooth13, selected: selected13 },
+        14: { default: tooth14, selected: selected14 },
+        15: { default: tooth15, selected: selected15 },
+        16: { default: tooth16, selected: selected16 },
+        17: { default: tooth17, selected: selected17 },
+        18: { default: tooth18, selected: selected18 },
+        19: { default: tooth19, selected: selected19 },
+        20: { default: tooth20, selected: selected20 },
+        21: { default: tooth21, selected: selected21 },
+        22: { default: tooth22, selected: selected22 },
+        23: { default: tooth23, selected: selected23 },
+        24: { default: tooth24, selected: selected24 },
+        25: { default: tooth25, selected: selected25 },
+        26: { default: tooth26, selected: selected26 },
+        27: { default: tooth27, selected: selected27 },
+        28: { default: tooth28, selected: selected28 },
+        29: { default: tooth29, selected: selected29 },
+        30: { default: tooth30, selected: selected30 },
+        31: { default: tooth31, selected: selected31 },
+        32: { default: tooth32, selected: selected32 },
+    };
 
+    const [selectedTeeth, setSelectedTeeth] = useState({});
+
+    const handleToothClick = (toothId) => {
+        setSelectedTeeth(prevState => ({
+            ...prevState,
+            [toothId]: !prevState[toothId]
+        }));
+    };
+
+    const renderTooth = (toothId) => {
+        const isSelected = selectedTeeth[toothId];
+        const imgSrc = isSelected ? toothImages[toothId].selected : toothImages[toothId].default;
+
+        return (
+            <div className="col tooth-container" key={toothId}>
+                <img src={imgSrc} className='tooth' alt="" />
+                <button className='btn tooth-button' onClick={() => handleToothClick(toothId)}>
+                    {toothId}
+                </button>
+            </div>
+        );
+    };
+
+    const renderTooth2 = (toothId) => {
+        const isSelected = selectedTeeth[toothId];
+        const imgSrc = isSelected ? toothImages[toothId].selected : toothImages[toothId].default;
+
+        return (
+            <div className="col tooth-container" key={toothId}>
+                <button className='btn tooth-button' onClick={() => handleToothClick(toothId)}>
+                    {toothId}
+                </button>
+                <img src={imgSrc} className='tooth' alt="" />
+                
+            </div>
+        );
+    };
     
 
     const [formData, setFormData] = useState({
@@ -225,150 +361,26 @@ const AddPatient = () => {
                 </div>
 
                 {/* tooth chart */}
+                <div className="tooth-chart">
                 <div className="row">
-                    {/* 16-9  and 17-24*/}
                     <div className="col-6">
-                        {/* 16-9 */}
                         <div className="row">
-                            <div className="col">
-                                16
-                                
-                            </div>
-                            <div className="col">
-                                15
-                                
-                            </div>
-                            <div className="col">
-                                14
-                                
-                            </div>
-                            <div className="col">
-                                13
-                                
-                            </div>
-                            <div className="col">
-                                12
-                                
-                            </div>
-                            <div className="col">
-                                11
-                                
-                            </div>
-                            <div className="col">
-                                10
-                                
-                            </div>
-                            <div className="col">
-                                9
-                            </div>
+                            {[16, 15, 14, 13, 12, 11, 10, 9].map(renderTooth)}
                         </div>
-                        {/* 17-24 */}
                         <div className="row">
-                            <div className="col">
-                                17
-                                
-                            </div>
-                            <div className="col">
-                                18
-                                
-                            </div>
-                            <div className="col">
-                                19
-                                
-                            </div>
-                            <div className="col">
-                                20
-                                
-                            </div>
-                            <div className="col">
-                                21
-                                
-                            </div>
-                            <div className="col">
-                                22
-                                
-                            </div>
-                            <div className="col">
-                                23
-                                
-                            </div>
-                            <div className="col">
-                                24
-                            </div>
+                            {[17, 18, 19, 20, 21, 22, 23, 24].map(renderTooth2)}
                         </div>
                     </div>
-                    {/* 8-1  and 25-32*/}
                     <div className="col-6">
-                        {/* 8-1 */}
                         <div className="row">
-                            <div className="col">
-                                8
-                                
-                            </div>
-                            <div className="col">
-                                    7
-                                
-                            </div>
-                            <div className="col">
-                                6
-                                
-                            </div>
-                            <div className="col">
-                                5
-                                
-                            </div>
-                            <div className="col">
-                                4
-                                
-                            </div>
-                            <div className="col">
-                                3
-                                
-                            </div>
-                            <div className="col">
-                                2
-                                
-                            </div>
-                            <div className="col">
-                                1
-                            </div>
+                            {[8, 7, 6, 5, 4, 3, 2, 1].map(renderTooth)}
                         </div>
-                        {/* 25-32 */}
                         <div className="row">
-                            <div className="col">
-                                25
-                                
-                            </div>
-                            <div className="col">
-                                26
-                                
-                            </div>
-                            <div className="col">
-                                27
-                                
-                            </div>
-                            <div className="col">
-                                28
-                                
-                            </div>
-                            <div className="col">
-                                29
-                                
-                            </div>
-                            <div className="col">
-                                30
-                                
-                            </div>
-                            <div className="col">
-                                31
-                                
-                            </div>
-                            <div className="col">
-                                32
-                            </div>
+                            {[25, 26, 27, 28, 29, 30, 31, 32].map(renderTooth2)}
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
             {/* end of row for tooth chart */}
 
@@ -378,7 +390,7 @@ const AddPatient = () => {
                 <div className="col-6">
                     <label htmlFor="" className="form-lavel labels">Dentist</label>
                     <select class="form-select" aria-label="Default select example" id="service" name="service_" value={formData.service_} onChange={handleChange}>
-                        <option value="" labels>Select a Service</option>
+                        <option value="" labels>Dentist</option>
                                 {services.map((service) => (
                                             <option key={service.value} value={service.value}>{service.label}</option>
                                         ))}

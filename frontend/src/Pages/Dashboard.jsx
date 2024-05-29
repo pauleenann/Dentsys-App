ba80f30e424c5ce4380dec351803db778281140d
lancewrt <91044173+lancewrt@users.noreply.github.com>
2024-05-29 15:27:30 +0800

    log in persistence

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,19 @@
-
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
 import AdminDash from '../Components/AdminDash/AdminDash'
 import AdminNavbar from '../Components/AdminNavbar/AdminNavbar'
+import { isAuthenticated } from 'C:/xampp/htdocs/Dentsys-App/frontend/src/Components/Auth';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated()) {
+    // Redirect to login if not authenticated
+    navigate('/admin');
+    }
+  }, [navigate]);
+
   return (
     <div>
       {/* <AdminNavbar></AdminNavbar> */}

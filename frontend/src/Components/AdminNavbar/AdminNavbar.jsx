db05bf923e1afeaf35d0bef72afceed6900ffde4
Pau <141435859+pauleenann@users.noreply.github.com>
2024-12-08 08:45:35 +0800

    fix sidebar

--- frontend/src/Components/AdminNavbar/AdminNavbar.jsx
+++ frontend/src/Components/AdminNavbar/AdminNavbar.jsx
@@ -5,73 +5,33 @@ import './AdminNavbar.css'
 
 const AdminNavbar = () => {
     const [accountType,setAccountType]=useState('')
+
+    //stores user account type in accountType useState
     useEffect(()=>{
-        
         setAccountType(localStorage.getItem('account_type'));
     },[])
 
   return (
-    <div>
-      <nav id="sidebar">
-                <div className='admin-navbar-container'>
-                <div className="admin-navbar">
-                <img src={logowhite} alt="" className='admin-navbar-logo'/>
-                {accountType==='admin'?<div className="admin-menu">
-                    <Link className='adminnav-link' to='/dashboard'>
-                        <div className="admin-nav-dashboard row">
-                            <div className="col-2 p-0">
-                            <i className="fa-solid fa-table-columns adminnav-menu"></i> 
-                            </div>
-                            <div className="col-10 p-0">
-                                <p className="admin-dashboard-text m-0 adminnav-menu">Dashboard</p>
-                            </div>                    
-                        </div>
-                </Link>
-                <Link className='adminnav-link' to='/appointment-list'>
-                    <div className="admin-nav-appointments row">
-                        <div className="col-2 p-0">
-                        <i className="fa-regular fa-calendar-check adminnav-menu"></i> 
-                        </div>
-                        <div className="col-10 p-0">
-                            <p className="admin-appointments-text m-0 adminnav-menu">Appointments</p>
-                        </div>
-                    </div>
-                </Link>
-                <Link className='adminnav-link' to='/invoice-list'>
-                    <div className="admin-nav-patients row">
-                        <div className="col-2 p-0">
-                        <i class="fa-solid fa-receipt adminnav-menu"></i>
-                        </div>
-                        <div className="col-10 p-0">
-                        <p className="admin-patients-text m-0 adminnav-menu">Invoices</p> 
-                        </div>
-                    </div>
-                </Link>
-                <Link className='adminnav-link' to='/patient-list'>
-                    <div className="admin-nav-patients row">
-                        <div className="col-2 p-0">
-                        <i className="fa-solid fa-hospital-user adminnav-menu"></i>   
-                        </div>
-                        <div className="col-10 p-0">
-                        <p className="admin-patients-text m-0 adminnav-menu">Patients</p> 
-                        </div>
-                    </div>
-                </Link>
-                </div>:
-                <Link className='adminnav-link-patient' to='/patient-list'>
-                    <div className="admin-nav-patients row">
-                        <div className="col-2 p-0">
-                        <i className="fa-solid fa-hospital-user adminnav-menu"></i>   
-                        </div>
-                        <div className="col-10 p-0">
-                        <span className="admin-patients-text m-0 adminnav-menu">Patients</span> 
-                        </div>
-                    </div>
-                </Link>}
-                
-            </div>
-            </div>
-            </nav>
+    <div className='sidebar'>
+        <img src={logowhite} alt="" className='sidebar-logo'/>
+        <div className="sidebar-menu-container">
+            <Link className={`sidebar-menu row ${accountType === 'dentist' ? 'hide-menu' : ''}`} to='/dashboard'>
+                <i className="fa-solid fa-table-columns col-2"></i> 
+                <p className="col-10 m-0">Dashboard</p>     
+            </Link>
+            <Link className={`sidebar-menu row ${accountType === 'dentist' ? 'hide-menu' : ''}`} to='/appointment-list'>
+                <i className="fa-regular fa-calendar-check col-2"></i> 
+                <p className="col-10 m-0">Appointments</p>     
+            </Link>
+            <Link className={`sidebar-menu row ${accountType === 'dentist' ? 'hide-menu' : ''}`} to='/invoice-list'>
+                <i className="fa-solid fa-receipt col-2"></i> 
+                <p className="col-10 m-0">Invoices</p>     
+            </Link>
+            <Link className={`sidebar-menu row ${accountType === 'dentist' ? 'hide-menu' : ''}`} to='/patient-list'>
+                <i className="fa-solid fa-hospital-user col-2"></i> 
+                <p className="col-10 m-0">Patients</p>     
+            </Link>
+        </div>
     </div>
   )
 }
